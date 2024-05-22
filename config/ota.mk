3827b8fcf13de337c42a58c3e1f500f673656286
Ali Hasan <ahb7671@gmail.com>
2024-05-22 13:30:23 +0500

    vendor: enable updates on unofficial
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -1,8 +1,9 @@
 # Updater
-ifeq ($(IS_OFFICIAL),true)
     PRODUCT_PACKAGES += \
         Updates
 
     PRODUCT_COPY_FILES += \
         vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
-endif
+
+#PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.custom.releasetype=unofficial
